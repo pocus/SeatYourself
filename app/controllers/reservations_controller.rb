66d94f47673dc6394f25eb973e0136c86657f02c
bshain <shainbarbara@gmail.com>
2014-02-08 16:25:12 -0500

    Can add reservation. Can delete reservations. Solution will check if seats are available.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,16 @@
 class ReservationsController < ApplicationController
   
   def index
+    @reservations = Reservation.all 
+    @seats_occupied = seats_occupied
   end
 
   def edit
   end
 
   def new
-    my_reso = Reservation.new
-
+    # @user = User.find_by(user_id)
+    @my_reso = Reservation.new
   end
 
   def show
@@ -16,24 +18,31 @@ class ReservationsController < ApplicationController
 
   def create
     @my_reso = Reservation.new(reservation_params)
-    if @my_reso.save
-        redirect_to reservations_path
-      else
+    if check_avail(@my_reso) == true
+      @my_reso.save
+      redirect_to reservations_path
+    else
       render :new
-    end
+    end 
   end
 
   def update
   end
 
   def destroy
+    my_reso = Reservation.find(params[:id])
+    my_reso.destroy
+    redirect_to reservations_path
+  end
+
+  
+  def check_avail(candidate_reservation)
+    candidate_reservation.guest_qty < (Restaurant.find_by(id: 1).totalseats - seats_occupied)
   end
 
   private 
   def reservation_params
-
     params.require(:reservation).permit(:guest_qty, :date,:time, :restaurant_id, :user_id)
-
   end
 
 end
