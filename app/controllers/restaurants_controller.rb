ebdd94a4d7f452c59d3bc48c9e533630c9004bd1
David Yip <dvd.yip@gmail.com>
2014-02-09 18:08:58 -0500

    CRUD functionality for restaurants added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,46 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all 
+    @restaurants = Restaurant.all
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update_attributes(restaurant_params)
+        redirect_to restaurants_path
+    else
+        render :edit
+    end
+
+
+  end
+
+
 
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+        redirect_to restaurants_path
+    else
+        render :new
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:address, :timeopening, :timeclosing, :menu, :pricerange, :category, :totalseats, :description, :name )
+  end
+
+
 end
