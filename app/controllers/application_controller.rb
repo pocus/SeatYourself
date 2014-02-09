5a4780d3f1654d49ed2ca7f3056333c41b77d561
David Yip <dvd.yip@gmail.com>
2014-02-08 19:32:27 -0500

    added additional hash symbol to access params - checks against time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
-  
-  
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def seats_occupied_at_hour
-    @reservations = Reservation.all 
+    @reservations = Reservation.all
      # candidate_restaurant_reservations = @reservations.where("restaurant_id='1'") #later make this restaurant_id
-     candidate_restaurant_reservations = @reservations.where("hour = ? AND restaurant_id = ?", params[:hour], params[:restaurant_id])
+     candidate_restaurant_reservations = @reservations.where("hour = ? AND restaurant_id = ?", params[:reservation][:hour], params[:reservation][:restaurant_id])
      result = 0
      candidate_restaurant_reservations.each do |c|
       result = c.guest_qty + result
