37bb717cb927949313483698f0614cafd57b9616
bshain <shainbarbara@gmail.com>
2014-02-09 17:42:27 -0500

    added restaurant index and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all 
   end
 
   def edit
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
